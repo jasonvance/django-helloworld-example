389a00073fccb290890ddac5c88d0564904bc33b
jasonvance <vance.jason@gmail.com>
2017-10-27 17:53:46 -0600

    Added Redis Server

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -57,7 +57,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'helloworld.wsgi.application'
 
-
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "helloworld"
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
